96374e401cd20165d799bc306b9fc7583c6457fd
joseca331 <blanco.gonzalez.jose.carlos@iescamas.es>
2025-09-16 21:03:00 +0200

    Add files via upload

---
+++ highestNumber.py
@@ -0,0 +1,15 @@
+def highest_num (num1, num2, num3):
+    if num1 >= num2 and num1 >=num3:
+        highest_num = num1
+        
+        
+        
+    else highest_num3
+    
+    return highest_num
+    
+    
+first_num = int("indroduce el primer numero: "))
+second_num = int("introduce el segundo numero: "))
+third_num = int("introduce el tercer numero: "))
+
