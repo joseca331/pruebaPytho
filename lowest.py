0cc8005c9075cd3d962a8a9fd21821085e8efcd9
joseca331 <blanco.gonzalez.jose.carlos@iescamas.es>
2025-09-16 20:21:11 +0200

    Add files via upload

---
+++ lowest.py
@@ -0,0 +1,11 @@
+num1 = int(input("Introduce el primer numero:"))
+num2 = int(input("Introduce el segundo numero:"))
+
+if num1 <= num2 :
+    lowest = num1
+    
+else:
+    lowest = num2
+    
+    
+print("el numero mas bajo es: " + str(lowest))
