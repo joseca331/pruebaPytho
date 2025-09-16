f5dc678dce2ddd250b1521436495dd0565cb315a
joseca331 <blanco.gonzalez.jose.carlos@iescamas.es>
2025-09-16 22:31:03 +0200

    highestNumber.py

--- highestNumber.py
+++ highestNumber.py
@@ -1,11 +1,10 @@
-def highest_num (num1, num2, num3):
-    if num1 >= num2 and num1 >=num3:
+def highest_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
         highest_num = num1
-        
-        
-        
-    else highest_num3
-    
+    elif num2 >= num1 and num2 >= num3:
+        highest_num = num2
+    else:
+        highest_num = num3
     return highest_num
     
     
@@ -13,3 +12,5 @@ first_num = int("indroduce el primer numero: "))
 second_num = int("introduce el segundo numero: "))
 third_num = int("introduce el tercer numero: "))
 
+
+print("El número más alto es:", highest_num(first_num, second_num, third_num))
